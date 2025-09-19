11e5ec0f7c272cf6bf84231255344b0774b5cdaf
thuyht1985 <hoangthithuy1985@gmail.com>
2025-09-19 18:45:03 +0700

    commit

---
+++ tests/ex8.js
@@ -0,0 +1,6 @@
+// In các số cách nhau 4 đơn vị từ 1 đến 100
+let result8 = [];
+for (let i = 1; i <= 100; i += 4) {
+  result8.push(i);
+}
+console.log(result8.join(', '));
--- tests/ex9.js
+++ tests/ex9.js
@@ -1,1 +1,6 @@
-.
+// In các số từ 25 về 12
+let result9 = [];
+for (let i = 25; i >= 12; i--) {
+  result9.push(i);
+}
+console.log(result9.join(', '));
